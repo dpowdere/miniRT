75c75cfb46c11d3193fcc5aafb9f0d31855ff6fb
Dorine Powdered <dpowdere@student.21-school.ru>
2021-03-12 11:08:32 +0300

    Parse resolution config option

--- src/rt_parse_resolution.c
+++ src/rt_parse_resolution.c
@@ -12,10 +12,29 @@
 
 #include <stddef.h>
 
+#include <libft.h>
+
 #include "minirt.h"
+
+#define BASE 10
 
 void	rt_parse_resolution(t_config_line *c, t_scene *scene)
 {
-	(void)c;
-	(void)scene;
+	int		width;
+	int		height;
+	char	*endptr;
+
+	if (scene->width != UNDEFINED || scene->height != UNDEFINED)
+		rt_parse_error(c, "Resolution can be specified only once");
+	if (c->n_segments != 3)
+		rt_parse_error(c, "Resolution config option has wrong number of args");
+	endptr = NULL;
+	width = ft_strtoi(c->segments[1], &endptr, BASE);
+	if (*endptr != '\0' || width < 1)
+		rt_parse_error(c, "Invalid config value for resolution width");
+	scene->width = width;
+	height = ft_strtoi(c->segments[2], &endptr, BASE);
+	if (*endptr != '\0' || height < 1)
+		rt_parse_error(c, "Invalid config value for resolution height");
+	scene->height = height;
 }
