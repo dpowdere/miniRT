b698f5cd18dd97b9ad04d0b2ffff30d2f2ea1c1b
Dorine Powdered <dpowdere@student.21-school.ru>
2021-03-13 21:39:26 +0300

    Parse and load sphere objects

--- src/rt_parse_sphere.c
+++ src/rt_parse_sphere.c
@@ -11,10 +11,29 @@
 /* ************************************************************************** */
 
 #include <stddef.h>
+#include <stdlib.h>
+
+#include <libft.h>
 
 #include "minirt.h"
 
 void	rt_parse_sphere(t_config_line *c)
 {
-	(void)c;
+	t_sphere	*sp;
+	t_list		*list_element;
+
+	if (c->n_segments != 4)
+		rt_parsing_error(c, "Sphere", "Wrong number of arguments");
+	if ((sp = (t_sphere *)malloc(sizeof(t_sphere))) == NULL)
+		rt_perror((void *)c, RT_CONFIG_LINE);
+	*(int *)&sp->type = RT_SPHERE;
+	sp->location = rt_parse_vector(c, 1, "Sphere location", NON_NORMALIZED);
+	sp->diameter = rt_parse_float(c, 2, "Sphere diameter");
+	sp->color = rt_parse_color(c, 3, "Sphere color");
+	if ((list_element = ft_lstnew(sp)) == NULL)
+	{
+		free(sp);
+		rt_perror((void *)c, RT_CONFIG_LINE);
+	}
+	ft_lstadd_back(&c->scene->objects, list_element);
 }
