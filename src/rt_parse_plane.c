23ada3d77982507f5e8806deaab2fc6b0aebdcc3
Dorine Powdered <dpowdere@student.21-school.ru>
2021-03-13 21:44:12 +0300

    Parse and load plane objects

--- src/rt_parse_plane.c
+++ src/rt_parse_plane.c
@@ -11,10 +11,29 @@
 /* ************************************************************************** */
 
 #include <stddef.h>
+#include <stdlib.h>
+
+#include <libft.h>
 
 #include "minirt.h"
 
 void	rt_parse_plane(t_config_line *c)
 {
-	(void)c;
+	t_plane	*pl;
+	t_list	*list_element;
+
+	if (c->n_segments != 4)
+		rt_parsing_error(c, "Plane", "Wrong number of arguments");
+	if ((pl = (t_plane *)malloc(sizeof(t_plane))) == NULL)
+		rt_perror((void *)c, RT_CONFIG_LINE);
+	*(int *)&pl->type = RT_PLANE;
+	pl->location = rt_parse_vector(c, 1, "Plane location", NON_NORMALIZED);
+	pl->orientation = rt_parse_vector(c, 2, "Plane orientation", NORMALIZED);
+	pl->color = rt_parse_color(c, 3, "Plane color");
+	if ((list_element = ft_lstnew(pl)) == NULL)
+	{
+		free(pl);
+		rt_perror((void *)c, RT_CONFIG_LINE);
+	}
+	ft_lstadd_back(&c->scene->objects, list_element);
 }
