dc5bc54cbee48b2afe2a82eb4ada8fdf3d6afb06
Dorine Powdered <dpowdere@student.21-school.ru>
2021-05-01 19:06:21 +0300

    Normalize

--- include/rt_quadratic.h
+++ include/rt_quadratic.h
@@ -6,7 +6,7 @@
 /*   By: dpowdere <dpowdere@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/01 22:34:09 by dpowdere          #+#    #+#             */
-/*   Updated: 2021/04/01 22:34:31 by dpowdere         ###   ########.fr       */
+/*   Updated: 2021/05/01 19:05:33 by dpowdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,21 @@ typedef struct s_quadratic_equation_roots
 	double	root1;
 	double	root2;
 }		t_roots;
+
+typedef struct s_moeller_trumbore
+{
+	t_vector	e1;
+	t_vector	e2;
+	t_vector	h;
+	t_vector	s;
+	t_vector	q;
+	t_scalar	a;
+	t_scalar	f;
+	t_scalar	u;
+	t_scalar	v;
+	t_scalar	t;
+	t_x			x;
+}		t_mt;
 
 double	rt_get_quadratic_root(t_roots r, int *is_inside, double limit);
 t_roots	rt_quadratic_equation(double a, double b, double c);
--- src/rt_triangle.c
+++ src/rt_triangle.c
@@ -6,7 +6,7 @@
 /*   By: dpowdere <dpowdere@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/12 02:07:34 by dpowdere          #+#    #+#             */
-/*   Updated: 2021/03/12 02:07:46 by dpowdere         ###   ########.fr       */
+/*   Updated: 2021/05/01 19:05:38 by dpowdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,41 +46,40 @@ void	rt_parse_triangle(t_config_line *c)
 ** The Moeller–Trumbore ray-triangle intersection algorithm, a fast method for
 ** calculating the intersection of a ray and a triangle in three dimensions
 ** without needing precomputation of the plane equation of the plane containing
-** the triangle.
+** the triangle. It uses the barycentric coordinates of an intersection point.
+**
+**   $  O + tD = (1 - u - v) V_0 + u V_1 + v V_2  $
+**
+** See https://cadxfem.org/inf/
+** Fast%20MinimumStorage%20RayTriangle%20Intersection.pdf
 */
 t_x	rt_triangle_intersection(t_ray ray, t_triangle *tr, double limit)
 {
-	const t_vector	e1 = vt_add(tr->p2, vt_inv(tr->p1));
-	const t_vector	e2 = vt_add(tr->p3, vt_inv(tr->p1));
-	const t_vector	h = vt_mul_cross(ray.orientation, e2);
-	const t_scalar	a = vt_mul_dot(e1, h);
-	t_x				x;
-	t_vector		s;
-	t_vector		q;
-	t_scalar		f;
-	t_scalar		u;
-	t_scalar		v;
-	t_scalar		t;
+	static t_mt	mt;
 
-	x = rt_get_no_intersection(ray, tr);
-	if (fabs(a) < EPS)
-		return (x);
-	f = 1. / a;
-	s = vt_add(ray.origin, vt_inv(tr->p1));
-	u = f * vt_mul_dot(s, h);
-	if (u < 0. || u > 1.)
-		return (x);
-	q = vt_mul_cross(s, e1);
-	v = f * vt_mul_dot(ray.orientation, q);
-	if (v < 0. || u + v > 1.)
-		return (x);
-	t = f * vt_mul_dot(e2, q);
-	if (t > EPS && t < limit)
+	mt.e1 = vt_add(tr->p2, vt_inv(tr->p1));
+	mt.e2 = vt_add(tr->p3, vt_inv(tr->p1));
+	mt.h = vt_mul_cross(ray.orientation, mt.e2);
+	mt.a = vt_mul_dot(mt.e1, mt.h);
+	mt.x = rt_get_no_intersection(ray, tr);
+	if (fabs(mt.a) < EPS)
+		return (mt.x);
+	mt.f = 1. / mt.a;
+	mt.s = vt_add(ray.origin, vt_inv(tr->p1));
+	mt.u = mt.f * vt_mul_dot(mt.s, mt.h);
+	if (mt.u < 0. || mt.u > 1.)
+		return (mt.x);
+	mt.q = vt_mul_cross(mt.s, mt.e1);
+	mt.v = mt.f * vt_mul_dot(ray.orientation, mt.q);
+	if (mt.v < 0. || mt.u + mt.v > 1.)
+		return (mt.x);
+	mt.t = mt.f * vt_mul_dot(mt.e2, mt.q);
+	if (mt.t > EPS && mt.t < limit)
 	{
-		x.point = vt_add(ray.origin, vt_mul_sc(ray.orientation, t));
-		x.color = tr->color;
+		mt.x.point = vt_add(ray.origin, vt_mul_sc(ray.orientation, mt.t));
+		mt.x.color = tr->color;
 	}
-	return (x);
+	return (mt.x);
 }
 
 void	rt_triangle_normal(t_x *x)
